57296cab1fd9930629a9beb72df06bb70e235afe
Devon Church <notifydevon@gmail.com>
2020-04-24 20:57:49 +1200

    feat: 🎸 add item display/injection

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <form @submit="handleNewItemSubmit">
+      <label for="new-item"></label>
+      <input id="new-item" v-model="newItem" type="text" />
+    </form>
+    <ul>
+      <li v-for="item in requestedItems" :key="item.id">{{ item.message }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data: function() {
+    return {
+      newItem: "",
+      itemIds: ["foo"],
+      items: {
+        foo: {
+          isComplete: false,
+          message: "My first task",
+        },
+      },
+    };
+  },
+  computed: {
+    requestedItems: function() {
+      return this.itemIds.map((itemId) => ({
+        ...this.items[itemId],
+        id: itemId,
+      }));
+    },
+  },
+  methods: {
+    handleNewItemSubmit: function(event) {
+      const itemId = `${Math.random()}`;
+      const item = { isComplete: true, message: this.newItem };
+
+      this.itemIds = [...this.itemIds, itemId];
+      this.items = { ...this.items, [itemId]: item };
+      this.newItem = "";
+
+      event.preventDefault();
+    },
+  },
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
