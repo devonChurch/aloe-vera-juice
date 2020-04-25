acf30f8a27508c030345a2cfefb841e1ce0cf325
Devon Church <notifydevon@gmail.com>
2020-04-25 15:06:07 +1200

    feat: 🎸 hide redundant UI when no items exist

--- src/App.vue
+++ src/App.vue
@@ -6,44 +6,46 @@
       <label for="new-item"></label>
       <input id="new-item" v-model="newItem" type="text" />
     </form>
-    <ul>
-      <li v-for="item in requestedItems" :key="item.id">
-        <label :for="'complete-' + item.id">Complete</label>
-        <input
-          type="checkbox"
-          :id="'complete-' + item.id"
-          :name="'complete-' + item.id"
-          v-model="item.isComplete"
-        />
-        {{ item.message }}
-        <button @click="handleRemoveClick(item.id)">Delete</button>
-      </li>
-    </ul>
-    <span>{{ totalActiveItems }}</span>
-    <label for="request-all">All</label>
-    <input
-      type="radio"
-      id="request-all"
-      name="request-type"
-      value="all"
-      v-model="requestedType"
-    />
-    <label for="request-active">Active</label>
-    <input
-      type="radio"
-      id="request-active"
-      name="request-type"
-      value="active"
-      v-model="requestedType"
-    />
-    <label for="request-completed">Completed</label>
-    <input
-      type="radio"
-      id="request-completed"
-      name="request-type"
-      value="completed"
-      v-model="requestedType"
-    />
+    <div v-show="hasAnyItems">
+      <ul>
+        <li v-for="item in requestedItems" :key="item.id">
+          <label :for="'complete-' + item.id">Complete</label>
+          <input
+            type="checkbox"
+            :id="'complete-' + item.id"
+            :name="'complete-' + item.id"
+            v-model="item.isComplete"
+          />
+          {{ item.message }}
+          <button @click="handleRemoveClick(item.id)">Delete</button>
+        </li>
+      </ul>
+      <span>{{ totalActiveItems }}</span>
+      <label for="request-all">All</label>
+      <input
+        type="radio"
+        id="request-all"
+        name="request-type"
+        value="all"
+        v-model="requestedType"
+      />
+      <label for="request-active">Active</label>
+      <input
+        type="radio"
+        id="request-active"
+        name="request-type"
+        value="active"
+        v-model="requestedType"
+      />
+      <label for="request-completed">Completed</label>
+      <input
+        type="radio"
+        id="request-completed"
+        name="request-type"
+        value="completed"
+        v-model="requestedType"
+      />
+    </div>
   </div>
 </template>
 
@@ -68,6 +70,9 @@ export default {
   computed: {
     requestedItems() {
       return this[`requestItems-${this.requestedType}`]();
+    },
+    hasAnyItems() {
+      return Boolean(this.items.length);
     },
     totalActiveItems() {
       const total = this["requestItems-active"]().length;
