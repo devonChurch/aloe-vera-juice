27f3d08eac3e64c9ea1a8507fd4bc5dfea3b0d1a
Devon Church <notifydevon@gmail.com>
2020-05-02 20:03:52 +1200

    feat: 🎸 add vue router scaffold

--- src/App.vue
+++ src/App.vue
@@ -1,160 +1,32 @@
 <template>
-  <main id="app">
-    <h1>{{ $t("message.heading.app") }}</h1>
-    <div v-show="hasAnyItems">
-      <label for="complete-all">{{ $t("message.label.complete.all") }}</label>
-      <input
-        type="checkbox"
-        id="complete-all"
-        name="complete-all"
-        :indeterminate.prop="hasPartialCompletedItems"
-        @change="handleCompleteAllChange"
-        v-model="isCompleteAllToggled"
-      />
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
-    <form @submit="handleNewItemSubmit">
-      <label for="new-item">{{ $t("message.label.add.one") }}</label>
-      <input id="new-item" v-model="newItemMessage" type="text" />
-    </form>
-    <div v-show="hasAnyItems">
-      <ul>
-        <li v-for="item in requestedItems" :key="item.id">
-          <label :for="'complete-' + item.id">{{
-            $t("message.label.complete.one")
-          }}</label>
-          <input
-            type="checkbox"
-            :id="'complete-' + item.id"
-            :name="'complete-' + item.id"
-            @change="handleCompleteItemChange"
-            v-model="item.isComplete"
-          />
-          {{ item.message }}
-          <button @click="handleRemoveClick(item.id)">
-            {{ $t("message.action.remove.one") }}
-          </button>
-        </li>
-      </ul>
-      <span>{{ $tc("message.label.active.all", totalActiveItems) }}</span>
-      <label for="request-all">{{ $t("message.label.filter.all") }}</label>
-      <input
-        type="radio"
-        id="request-all"
-        name="request-type"
-        value="all"
-        v-model="requestedType"
-      />
-      <label for="request-active">{{
-        $t("message.label.filter.active")
-      }}</label>
-      <input
-        type="radio"
-        id="request-active"
-        name="request-type"
-        value="active"
-        v-model="requestedType"
-      />
-      <label for="request-completed">{{
-        $t("message.label.filter.completed")
-      }}</label>
-      <input
-        type="radio"
-        id="request-completed"
-        name="request-type"
-        value="completed"
-        v-model="requestedType"
-      />
-      <button v-show="hasAnyCompletedItems" @click="handleClearCompletedClick">
-        {{ $t("message.action.remove.completed") }}
-      </button>
-    </div>
-  </main>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-export default {
-  name: "App",
-  data() {
-    return {
-      isCompleteAllToggled: false,
-      requestedType: "all",
-      newItemMessage: "",
-      items: [],
-    };
-  },
-  computed: {
-    requestedItems() {
-      return this[`requestItems-${this.requestedType}`]();
-    },
-    hasAnyItems() {
-      return Boolean(this["requestItems-all"]().length);
-    },
-    hasAnyCompletedItems() {
-      return Boolean(this["requestItems-completed"]().length);
-    },
-    hasPartialCompletedItems() {
-      const totalItems = this["requestItems-all"]().length;
-      const totalCompletedItems = this["requestItems-completed"]().length;
-      return totalCompletedItems && totalCompletedItems < totalItems;
-    },
-    totalActiveItems() {
-      return this["requestItems-active"]().length;
-    },
-  },
-  watch: {
-    items() {
-      this.setCompleteAllToggle();
-    },
-  },
-  methods: {
-    handleNewItemSubmit(event) {
-      if (this.newItemMessage !== "") {
-        const item = {
-          id: `${Math.random()}`,
-          isComplete: false,
-          message: this.newItemMessage,
-        };
-
-        this.items = [...this.items, item];
-        this.newItemMessage = "";
-      }
-
-      event.preventDefault();
-    },
-    handleRemoveClick(removeId) {
-      this.items = this.items.filter(({ id }) => id !== removeId);
-    },
-    handleClearCompletedClick() {
-      this.items = this.items.filter(({ isComplete }) => !isComplete);
-    },
-    handleCompleteAllChange() {
-      this.items = [...this.items].map((item) => ({
-        ...item,
-        isComplete: this.isCompleteAllToggled,
-      }));
-    },
-    handleCompleteItemChange() {
-      this.setCompleteAllToggle();
-    },
-    "requestItems-all"() {
-      return this.items;
-    },
-    "requestItems-active"() {
-      return this.items.filter(({ isComplete }) => !isComplete);
-    },
-    "requestItems-completed"() {
-      return this.items.filter(({ isComplete }) => isComplete);
-    },
-    setCompleteAllToggle() {
-      this.isCompleteAllToggled = !this.items.some(
-        ({ isComplete }) => !isComplete
-      );
-    },
-  },
-  components: {},
-};
-</script>
-
 <style>
-/** ... */
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueI18n from "vue-i18n";
 
 import App from "./App.vue";
+import router from './router'
 
 Vue.config.productionTip = false;
 
@@ -55,5 +56,6 @@ const i18n = new VueI18n({
 
 new Vue({
   i18n,
-  render: (h) => h(App),
+  router,
+  render: (h) => h(App)
 }).$mount("#app");
